14127656d5795143885578450cd50ee49192db15
Marthadrake <drake_233@msn.com>
2019-08-17 17:42:30 -0600

    test functions

--- game.js
+++ game.js
@@ -11,14 +11,53 @@ let hit_div = document.getElementById("hit");
 let kick_div = document.getElementById("kick")
 let slap_div = document.getElementById("slap")
 
-hit_div.addEventListener('click', function () {
-  console.log("hey you clicked on hit");
-})
+function getGamerTwoChoice() {
+  let choices = ['hit', 'kick', 'slap'];
+  let randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
 
-kick_div.addEventListener('click', function () {
-  console.log("hey you clicked on kick");
-})
 
-slap_div.addEventListener('click', function () {
-  console.log("hey you clicked on slap");
-})
+function game(gamerOneChoice) {
+  let gamerTwoChoice = getGamerTwoChoice();
+  switch (gamerOneChoice + gamerTwoChoice) {
+    case "hitslap":
+    case "slapkick":
+    case "kickhits":
+      console.log("user gamer 1 wins.");
+      break;
+    case "hitslap":
+    case "slapkick":
+    case "kickhits":
+      console.log("user gamer 2 wins");
+      break;
+  }
+}
+
+
+
+
+
+
+function game(gamerOneChoice) {
+  console.log("what you " + gamerOneChoice);
+
+}
+
+function main() {
+
+  hit_div.addEventListener('click', function () {
+    game("hit");
+  })
+
+  kick_div.addEventListener('click', function () {
+    game("kick");
+  })
+
+  slap_div.addEventListener('click', function () {
+    game("slap");
+  })
+
+}
+
+main();
