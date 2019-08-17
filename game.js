08f8b8b745e072ccbaadd5298634b5b780727e66
Marthadrake <drake_233@msn.com>
2019-08-17 16:13:06 -0600

    1st test

--- game.js
+++ game.js
@@ -1,10 +1,24 @@
 let gamerOne = 0;
 let gamerTwo = 0;
+
 let gamerOnehealth_span = document.getElementById("gamerOne-health");
 let gamerTwohealth_span = document.getElementById("gamerTwo-health");
+
 let cardBody_div = document.querySelector(".card-body");
 let results_div = document.querySelector(".results");
-let hit_div = document.querySelectorAll(".hit");
-let kick_div = document.querySelectorAll(".kick")
-let slap_div = document.querySelectorAll(".slap")
 
+let hit_div = document.getElementById("hit");
+let kick_div = document.getElementById("kick")
+let slap_div = document.getElementById("slap")
+
+hit_div.addEventListener('click', function () {
+  console.log("hey you clicked on hit");
+})
+
+kick_div.addEventListener('click', function () {
+  console.log("hey you clicked on kick");
+})
+
+slap_div.addEventListener('click', function () {
+  console.log("hey you clicked on slap");
+})
