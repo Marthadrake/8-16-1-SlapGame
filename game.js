59909e8424c0798871bc35990af77fb0e3cdbf54
Marthadrake <drake_233@msn.com>
2019-08-17 15:49:03 -0600

    adding JS

---
+++ game.js
@@ -0,0 +1,10 @@
+let gamerOne = 0;
+let gamerTwo = 0;
+let gamerOnehealth_span = document.getElementById("gamerOne-health");
+let gamerTwohealth_span = document.getElementById("gamerTwo-health");
+let cardBody_div = document.querySelector(".card-body");
+let results_div = document.querySelector(".results");
+let hit_div = document.querySelectorAll(".hit");
+let kick_div = document.querySelectorAll(".kick")
+let slap_div = document.querySelectorAll(".slap")
+
