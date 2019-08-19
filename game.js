ebf1056c49b5274043ae442e2cf633283dabf1bc
Marthadrake <drake_233@msn.com>
2019-08-19 09:35:21 -0600

    mixup

---
+++ game.js
@@ -0,0 +1,47 @@
+let health = 100
+let game = 0
+
+function slap() {
+  if (health = 100) {
+    return 5
+  } else if (health <= 10)
+    return 2
+}else if (health)
+}
+function hit() {
+
+}
+
+function kick() {
+
+}
+
+
+alert("health");
+
+
+let gamerOneLabel = 0
+let gamerTwoLabel = 0
+
+let gamerOneHealth = document.getElementById('gameOne-health')
+let gamerTwoHealth = document.getElementById('gameTwo-health')
+
+let attacksElem = document.getElementById('attacks')
+
+
+
+
+
+// function getGamerOneHealth() {
+//   let choices = ['hit', 'kick', 'slap']
+//   let randomNumber = Math.floor(Math.random() * 5);
+//   return choices[randomNumbers];
+
+//   function game(gamerChoice) {
+
+
+
+function draw() {
+
+}
+
