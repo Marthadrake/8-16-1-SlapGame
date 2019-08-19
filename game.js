a56453039ea2fef47901daa09015d44427e9d105
Marthadrake <drake_233@msn.com>
2019-08-19 10:00:25 -0600

    test

--- game.js
+++ game.js
@@ -2,25 +2,25 @@ let health = 100
 let game = 0
 
 function slap() {
-  let slap = 5
-  for (let i = 99; i < Number.length;)
-      
+  for (let i = 99; i < -1; i++);
+
 }
 
 function hit() {
-
+  let hit = 10
 }
 
 function kick() {
-
+  let kick = 15
 }
 
 function gamerOnehealth() {
+  let gameOnehealth = 0
 
 }
 
 function gamerTwwohealth() {
-
+  let gameTwohealth = 0
 }
 
 alert("health");
@@ -29,8 +29,8 @@ alert("health");
 let gamerOneLabel = 0
 let gamerTwoLabel = 0
 
-let gamerOneHealth = document.getElementById('gameOne-health')
-let gamerTwoHealth = document.getElementById('gameTwo-health')
+let gamerOneHealth = document.getElementById('gameOnehealth')
+let gamerTwoHealth = document.getElementById('gameTwohealth')
 
 let attacksElem = document.getElementById('attacks')
 
